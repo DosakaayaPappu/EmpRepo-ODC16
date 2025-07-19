497d0f39a840d1520c79003e271ac7f3d10f4f2f
Pachipulusu Venkata Sreeram <pachipulusu.venka@hcltech.com>
2025-07-19 13:00:15 +0530

    I am cleared conflicts

--- MyFirstEmpProject/src/com/hcltech/client/EmpClient.java
+++ MyFirstEmpProject/src/com/hcltech/client/EmpClient.java
@@ -5,6 +5,6 @@ public class EmpClient {
 		System.out.println("Hello World");
 		System.out.println("Line 1");
 		System.out.println("hi io am from ");
-		System.out.println("This is for the line 4th");
+		System.out.println("This is for the line 4");
 	}
 }
